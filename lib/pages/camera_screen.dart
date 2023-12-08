aa90271a1f7836c4d7a582225046766a84de8dc1
DeanAvram <dean6198@gmail.com>
2023-12-08 10:46:25 +0200

    Added taking camera option

--- lib/pages/camera_screen.dart
+++ lib/pages/camera_screen.dart
@@ -1,5 +1,17 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'dart:io';
+import 'dart:convert';
+import 'package:image_picker/image_picker.dart';
+import 'package:map_app/pages/image_screen.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'dart:typed_data';
+import 'package:http_parser/http_parser.dart';
+
+
 
 class CameraScreen extends StatefulWidget {
   final List<CameraDescription>? cameras;
@@ -11,32 +23,51 @@ class CameraScreen extends StatefulWidget {
 
 class _CameraScreenState extends State<CameraScreen> {
   late CameraController _cameraController;
+  late Future<void> _initializeControllerFuture;
+  File? _image;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
-    initCamera(widget.cameras![0]);
+    _cameraController =
+        CameraController(widget.cameras![0], ResolutionPreset.high);
+    _initializeControllerFuture = _cameraController.initialize();
   }
 
   @override
   void dispose() {
-    // Dispose of the controller when the widget is disposed.
     _cameraController.dispose();
     super.dispose();
   }
 
-  Future initCamera(CameraDescription cameraDescription) async {
-    _cameraController =
-        CameraController(cameraDescription, ResolutionPreset.high);
-    try {
-      await _cameraController.initialize().then((_) {
-        if (!mounted) return;
-        setState(() {});
-      });
-    } on CameraException catch (e) {
-      debugPrint("camera error $e");
-    }
+  Future getImage() async {
+    //choose image from device
+    final image = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = File(image!.path);
+    });
   }
+
+  /*void uploadImage(String imagePath) async {
+    try{
+      String email = 'user@gmail.com';
+      String password = 'Aabcd1234!';
+      Uri url = Uri.parse('${dotenv.env['BASE_URL']}/predict?email=$email&password=$password');
+      var request = http.MultipartRequest("POST", url);
+      Uri path = Uri.parse(imagePath);
+      File f = await toFile
+      print("fgg");
+      request.files.add(http.MultipartFile.fromBytes('file', await File.fromUri(path).readAsBytes(), contentType: MediaType('image', 'jpeg')));
+      request.send().then((response) {
+        if (response.statusCode == 200) print("Uploaded!");
+      });
+    }catch(e){
+      print(e);
+    }
+}*/
+  
+
 
   @override
   Widget build(BuildContext context) {
@@ -50,9 +81,50 @@ class _CameraScreenState extends State<CameraScreen> {
         ),
       ),
       body: SafeArea(
-        child: _cameraController.value.isInitialized
-            ? CameraPreview(_cameraController)
-            : const Center(child: CircularProgressIndicator()),
+        child: Column(
+          children: [
+            Expanded(
+              child: FutureBuilder<void>(
+                future: _initializeControllerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return CameraPreview(_cameraController);
+                  } else {
+                    return const Center(child: CircularProgressIndicator());
+                  }
+                },
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: ()async{
+                    try {
+                      // Ensure that the camera is initialized.
+                        await _initializeControllerFuture;
+
+                        // Attempt to take a picture and then get the location
+                        // where the image file is saved.
+                        final image = await _cameraController.takePicture();
+                        print(image.path);
+                        if (mounted){
+                          Navigator.pop(context);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => ImageScreen(imagePath: image.path)));
+                        }  
+                        //uploadImage(image.path);
+                      } catch (e) {
+                        // If an error occurs, log the error to the console.
+                        print(e);
+                      }
+                    
+                  },
+                  child: const Icon(Icons.camera),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
